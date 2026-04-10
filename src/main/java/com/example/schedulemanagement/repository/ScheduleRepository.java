67d9aacfe2f5c8b9532256ac06fdb6489d572373
minkim11 <alsgur4772@gmail.com>
2026-04-10 16:08:04 +0900

    Feat : lv2. 일정 전체 조회 기능 추가
    작성자명 기준으로 일정 목록 전체 조회
    작성자명 필수x

--- src/main/java/com/example/schedulemanagement/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedulemanagement/repository/ScheduleRepository.java
@@ -3,5 +3,8 @@ package com.example.schedulemanagement.repository;
 import com.example.schedulemanagement.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
+    List<Schedule> findAllByUserName(String userName);
 }
--- src/main/java/com/example/schedulemanagement/service/ScheduleService.java
+++ src/main/java/com/example/schedulemanagement/service/ScheduleService.java
@@ -2,11 +2,14 @@ package com.example.schedulemanagement.service;
 
 import com.example.schedulemanagement.dto.CreateScheduleRequest;
 import com.example.schedulemanagement.dto.CreateScheduleResponse;
+import com.example.schedulemanagement.dto.ReadScheduleResponse;
 import com.example.schedulemanagement.entity.Schedule;
 import com.example.schedulemanagement.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,5 +33,24 @@ public class ScheduleService {
                 savedSchedule.getCreatedAt(),
                 savedSchedule.getModifiedAt()
                 );
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReadScheduleResponse> readAllSchedules(String userName) {
+        List<Schedule> schedules;
+        if (userName == null) {
+            schedules = scheduleRepository.findAll();
+        } else {
+            schedules = scheduleRepository.findAllByUserName(userName);
+        }
+        return schedules.stream()
+                .map(schedule -> new ReadScheduleResponse(
+                    schedule.getScheduleName(),
+                    schedule.getScheduleId(),
+                    schedule.getDescription(),
+                    schedule.getUserName(),
+                    schedule.getCreatedAt(),
+                    schedule.getModifiedAt()))
+                .toList();
     }
 }
