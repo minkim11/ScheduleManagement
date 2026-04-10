94afbad194295005fb05a74c01612658ef2ac36e
minkim11 <alsgur4772@gmail.com>
2026-04-10 20:01:26 +0900

    Feat : lv4. 일정 삭제 기능 추가
    비밀번호 맞을 경우 삭제
    주석 추가

--- src/main/java/com/example/schedulemanagement/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanagement/controller/ScheduleController.java
@@ -15,6 +15,7 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    // 일정 생성
     @PostMapping("/schedules")
     public ResponseEntity<CreateScheduleResponse> createSchedule(
             @RequestBody CreateScheduleRequest request
@@ -22,13 +23,16 @@ public class ScheduleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.createSchedule(request));
     }
 
+    // 일정 전체 조회
     @GetMapping("/schedules")
     public ResponseEntity<List<ReadScheduleResponse>> readAllSchedules(
+            //작성자명 기준으로 일정 목록 조회, 조회 조건에 없을 시 전체 조회
             @RequestParam(name = "username", required = false) String userName
     ) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.readAllSchedules(userName));
     }
 
+    // 일정 선택 조회
     @GetMapping("/schedules/{scheduleId}")
     public ResponseEntity<ReadScheduleResponse> readOneSchedule(
             @PathVariable Long scheduleId
@@ -36,11 +40,22 @@ public class ScheduleController {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.readOneSchedule(scheduleId));
     }
 
+    // 일정 수정(일정명, 작성자명만 수정 가능)
     @PatchMapping("/schedules/{scheduleId}")
     public ResponseEntity<UpdateScheduleResponse> updateSchedule(
             @PathVariable Long scheduleId,
             @RequestBody UpdateScheduleRequest request
     ) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.updateSchedule(scheduleId, request));
+    }
+
+    // 일정 삭제
+    @DeleteMapping("/schedules/{scheduleId}")
+    public ResponseEntity<Void> deleteSchedule(
+            @PathVariable Long scheduleId,
+            @RequestBody DeleteScheduleRequest request
+    ) {
+        scheduleService.deleteSchedule(scheduleId, request);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
